87945eb4434a84afa09395838e5ac9aacc86a0e3
David Atherton <davidatherton92@gmail.com>
2020-08-17 16:32:17 +0100

    Add version cmd-line arg

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include <cstring>
+
 int main(int argc, char **argv)
 {
-  std::cout << "Hello, Jenkins!" << std::endl;
+	if (argc == 2 && strcmp(argv[1], "version") == 0)
+	{
+		std::cout << "VERSION 1.0.0" << std::endl;
+		return EXIT_SUCCESS;
+	}
+	
+	std::cout << "Hello, Jenkins!" << std::endl;
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
