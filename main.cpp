bba6592e08561546a1edf10493dde373d6dfa01f
David Atherton <davidatherton92@gmail.com>
2020-08-17 20:14:21 +0100

    Output git version info

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 #include <cstring>
 
+const char* getGITVersion()
+{	return 
+#include "git_version.in"
+;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc == 2 && strcmp(argv[1], "version") == 0)
 	{
-		std::cout << "VERSION 1.0.0" << std::endl;
+		std::cout << "GIT Info: " << getGITVersion() << "\n";
 		return EXIT_SUCCESS;
 	}
 	
